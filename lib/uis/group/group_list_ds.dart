2cd23ca336421131371d93cbe39d75faf82184a5
catalunha <catalunha.mj@gmail.com>
2020-07-30 12:09:00 -0300

    inserir separação  por module

--- lib/uis/group/group_list_ds.dart
+++ lib/uis/group/group_list_ds.dart
@@ -6,7 +6,7 @@ import 'package:peat/models/plataform_model.dart';
 import 'package:peat/models/user_model.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class GroupListDS extends StatelessWidget {
+class GroupListDS extends StatefulWidget {
   final List<GroupModel> groupList;
   final List<ModuleModel> moduleList;
   final List<PlataformModel> plataformList;
@@ -21,11 +21,17 @@ class GroupListDS extends StatelessWidget {
     this.plataformList,
     this.userModel,
   }) : super(key: key);
+
+  @override
+  _GroupListDSState createState() => _GroupListDSState();
+}
+
+class _GroupListDSState extends State<GroupListDS> {
   String moduleIdCodigo(String moduleId) {
     String _return;
     if (moduleId != null) {
       ModuleModel moduleModel =
-          moduleList.firstWhere((element) => element.id == moduleId);
+          widget.moduleList.firstWhere((element) => element.id == moduleId);
       _return = moduleModel.codigo;
     }
     return _return + ' || ' + moduleId.substring(0, 5);
@@ -34,8 +40,8 @@ class GroupListDS extends StatelessWidget {
   String plataformIdCodigo(String plataformId) {
     String _return;
     if (plataformId != null) {
-      PlataformModel plataformModel =
-          plataformList.firstWhere((element) => element.id == plataformId);
+      PlataformModel plataformModel = widget.plataformList
+          .firstWhere((element) => element.id == plataformId);
       _return = plataformModel.codigo;
     }
     return _return + ' || ' + plataformId.substring(0, 5);
@@ -43,60 +49,123 @@ class GroupListDS extends StatelessWidget {
 
   String userIdData(String userId) {
     String _return = 'Erro no userId';
-    if (userId == userModel.id) {
-      _return = '${userModel.displayName} || ${userModel.id.substring(0, 5)}';
+    if (userId == widget.userModel.id) {
+      _return =
+          '${widget.userModel.displayName} || ${widget.userModel.id.substring(0, 5)}';
     }
     return _return;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Lista com ${groupList.length} grupos'),
-      ),
-      body: ListView.builder(
-        itemCount: groupList.length,
-        itemBuilder: (context, index) {
-          final group = groupList[index];
-          return ListTile(
-            selected: group.arquived,
-            title: Text('${group.codigo}'),
-            subtitle: Text(
-                '\nnumber: ${group.number}\ndescription: ${group.description}\nstartCourse: ${DateFormat('yyyy-MM-dd HH:mm').format(group.startCourse)}\nendCourse: ${DateFormat('yyyy-MM-dd HH:mm').format(group.endCourse)}\nlocalCourse: ${group.localCourse}\nurlFolder: ${group.urlFolder}\nurlPhoto: ${group.urlPhoto}\nopened: ${group.opened}\nsuccess: ${group.success}\narquived: ${group.arquived}\nuserId: ${userIdData(group.userId)}\nplataformId: ${plataformIdCodigo(group.plataformId)}\nuserDateTimeOnBoard: ${DateFormat('yyyy-MM-dd').format(group.userDateTimeOnBoard)}\nmoduleId: ${moduleIdCodigo(group.moduleId)}\nworkerIdList: ${group.workerIdList != null ? group.workerIdList.length : null} '),
-            leading: IconButton(
-              icon: Icon(Icons.link),
-              tooltip: group.urlPhoto,
-              onPressed: () async {
-                if (group?.urlPhoto != null) {
-                  if (await canLaunch(group.urlPhoto)) {
-                    await launch(group.urlPhoto);
-                  }
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Lista com ${widget.groupList.length} grupos'),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                text: 'Abertos',
+              ),
+              Tab(
+                text: 'Fechados',
+              )
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            ListView.builder(
+              itemCount: widget.groupList.length,
+              itemBuilder: (context, index) {
+                final group = widget.groupList[index];
+                if (group.opened) {
+                  return ListTile(
+                    selected: group.arquived,
+                    title: Text('${group.codigo}'),
+                    subtitle: Text(
+                        '\nnumber: ${group.number}\ndescription: ${group.description}\nstartCourse: ${DateFormat('yyyy-MM-dd HH:mm').format(group.startCourse)}\nendCourse: ${DateFormat('yyyy-MM-dd HH:mm').format(group.endCourse)}\nlocalCourse: ${group.localCourse}\nurlFolder: ${group.urlFolder}\nurlPhoto: ${group.urlPhoto}\nopened: ${group.opened}\nsuccess: ${group.success}\narquived: ${group.arquived}\nuserId: ${userIdData(group.userId)}\nplataformId: ${plataformIdCodigo(group.plataformId)}\nuserDateTimeOnBoard: ${DateFormat('yyyy-MM-dd').format(group.userDateTimeOnBoard)}\nmoduleId: ${moduleIdCodigo(group.moduleId)}\nworkerIdList: ${group.workerIdList != null ? group.workerIdList.length : null} '),
+                    leading: IconButton(
+                      icon: Icon(Icons.link),
+                      tooltip: group.urlPhoto,
+                      onPressed: () async {
+                        if (group?.urlPhoto != null) {
+                          if (await canLaunch(group.urlPhoto)) {
+                            await launch(group.urlPhoto);
+                          }
+                        }
+                      },
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.link),
+                      tooltip: group.urlFolder,
+                      onPressed: () async {
+                        if (group?.urlFolder != null) {
+                          if (await canLaunch(group.urlFolder)) {
+                            await launch(group.urlFolder);
+                          }
+                        }
+                      },
+                    ),
+                    onTap: () {
+                      widget.onEditGroupCurrent(group.id);
+                    },
+                  );
+                } else {
+                  return Container();
                 }
               },
             ),
-            trailing: IconButton(
-              icon: Icon(Icons.link),
-              tooltip: group.urlFolder,
-              onPressed: () async {
-                if (group?.urlFolder != null) {
-                  if (await canLaunch(group.urlFolder)) {
-                    await launch(group.urlFolder);
-                  }
+            ListView.builder(
+              itemCount: widget.groupList.length,
+              itemBuilder: (context, index) {
+                final group = widget.groupList[index];
+                if (!group.opened) {
+                  return ListTile(
+                    selected: group.arquived,
+                    title: Text('${group.codigo}'),
+                    subtitle: Text(
+                        '\nnumber: ${group.number}\ndescription: ${group.description}\nstartCourse: ${DateFormat('yyyy-MM-dd HH:mm').format(group.startCourse)}\nendCourse: ${DateFormat('yyyy-MM-dd HH:mm').format(group.endCourse)}\nlocalCourse: ${group.localCourse}\nurlFolder: ${group.urlFolder}\nurlPhoto: ${group.urlPhoto}\nopened: ${group.opened}\nsuccess: ${group.success}\narquived: ${group.arquived}\nuserId: ${userIdData(group.userId)}\nplataformId: ${plataformIdCodigo(group.plataformId)}\nuserDateTimeOnBoard: ${DateFormat('yyyy-MM-dd').format(group.userDateTimeOnBoard)}\nmoduleId: ${moduleIdCodigo(group.moduleId)}\nworkerIdList: ${group.workerIdList != null ? group.workerIdList.length : null} '),
+                    leading: IconButton(
+                      icon: Icon(Icons.link),
+                      tooltip: group.urlPhoto,
+                      onPressed: () async {
+                        if (group?.urlPhoto != null) {
+                          if (await canLaunch(group.urlPhoto)) {
+                            await launch(group.urlPhoto);
+                          }
+                        }
+                      },
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(Icons.link),
+                      tooltip: group.urlFolder,
+                      onPressed: () async {
+                        if (group?.urlFolder != null) {
+                          if (await canLaunch(group.urlFolder)) {
+                            await launch(group.urlFolder);
+                          }
+                        }
+                      },
+                    ),
+                    onTap: () {
+                      widget.onEditGroupCurrent(group.id);
+                    },
+                  );
+                } else {
+                  return Container();
                 }
               },
             ),
-            onTap: () {
-              onEditGroupCurrent(group.id);
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          onEditGroupCurrent(null);
-        },
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            widget.onEditGroupCurrent(null);
+          },
+        ),
       ),
     );
   }
